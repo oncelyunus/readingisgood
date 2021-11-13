06d1ba1fc10df32796bb6f16a4cad02c662e25e7
Yunus Oncel <yunusoncel@gmail.com>
2021-11-13 18:22:59 +0300

    postman collection

--- src/main/java/com/getir/readingisgood/dto/response/StatResponseDTO.java
+++ src/main/java/com/getir/readingisgood/dto/response/StatResponseDTO.java
@@ -1,5 +1,6 @@
 package com.getir.readingisgood.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Data;
 import lombok.experimental.Accessors;
@@ -10,6 +11,7 @@ import java.math.BigDecimal;
 @Data
 @Accessors(chain = true)
 @JsonPropertyOrder({ "month", "totalOrderCount", "totalBookCount", "totalPurchasedAmount"})
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class StatResponseDTO implements Serializable {
         private String month;
         private Integer totalOrderCount;
